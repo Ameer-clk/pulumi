b47f682bcb08429df3628087dc1bc81cd595671b
Ameer ALi <114929844+Ameer-clk@users.noreply.github.com>
2023-08-19 17:45:16 +0400

    Update main.py

--- pulumi-python/main.py
+++ pulumi-python/main.py
@@ -1,67 +1,65 @@
-import pulumi 
-import pulumi aws as aws 
+import pulumi
+import pulumi_aws as aws
 
 # Create vpc
 vpc = aws.ec2.Vpc("my_vpc",
     cidr_block="172.31.0.0/16")
 
 # Create public subnet
-public_subnet = aws.ec2.Subnet("public_subnet", # you typed Subnet correctly here
-    vpc_id='my_vpc.id',
+public_subnet = aws.ec2.Subnet("public_subnet",
+    vpc_id=vpc.id,
     availability_zone='us-east-1a',
     cidr_block="172.31.10.0/24")
 
 # Create private subnet
-private_subnet = aws.ec2.Subnet("private_subnet", # You typed aws.ec2.subnet it should be aws.ec2.Subnet
-    vpc_id='my_vpc.id',
+private_subnet = aws.ec2.Subnet("private_subnet",
+    vpc_id=vpc.id,
     availability_zone='us-east-1b',
     cidr_block="172.31.20.0/24")
 
 # Create InternetGateway
 internet_gateway = aws.ec2.InternetGateway("my_ig",
-    vpc_id='my_vpc.id',)
+    vpc_id=vpc.id)
 
 # Create RouteTable
 public_route_table = aws.ec2.RouteTable("public_route_table",
-    vpc_id='my_vpc.id',)
+    vpc_id=vpc.id)
 
-private_route_table = aws.ec2.RouteTable("private_route_table", # You typed aws.ec.RouteTable it should be aws.ec2.RouteTable
-    vpc_id='my_vpc.id',)
+private_route_table = aws.ec2.RouteTable("private_route_table",
+    vpc_id=vpc.id)
 
 # Create Public route
 public_route = aws.ec2.Route("public_route",
-    route_table_id=public_route_table.id, # That should be route_id instead of route_table
-    destination_cidr_block="0.0.0.0/0", # That should be destination_cidr_block instead of destination_block
-    gateway_id='my_ig.id',) # You typed my_ig.id it should be internet_gateway.id
+    route_table_id=public_route_table.id,
+    destination_cidr_block="0.0.0.0/0",
+    gateway_id=internet_gateway.id)
 
 # Create private route
 private_route = aws.ec2.Route("private_route",
-        route_table_id=private_route_table.id, # That should be route_id instead of route_table
-        destination_cidr_block="0.0.0.0/0", # That should be destination_cidr_block instead of destination_block
-        gateway_id='my_ig.id',) # You typed my_ig.id it should be internet_gateway.id
+    route_table_id=private_route_table.id,
+    destination_cidr_block="0.0.0.0/0",
+    gateway_id=internet_gateway.id)
 
-# Public Route table association 
-public_association = aws.ec2.RouteTableAssociation("publicassociation",
+# Public Route table association
+public_association = aws.ec2.RouteTableAssociation("public_association",
     subnet_id=public_subnet.id,
-    route_table_id=public_route_table.id) # You typed public_routetable.id it should be public_route_table.id
- 
-# Private Route table association 
-private_association= aws.ec2.RouteTableAssociation("privateassociation",
+    route_table_id=public_route_table.id)
+
+# Private Route table association
+private_association = aws.ec2.RouteTableAssociation("private_association",
     subnet_id=private_subnet.id,
     route_table_id=private_route_table.id)
 
 # Create an AWS Security Group
-newsg = aws.ec2.SecurityGroup("newsg", # You typed resource_name="newsg" it should be "newsg"
+newsg = aws.ec2.SecurityGroup("newsg",
     description='My Security Group',
-    ingress=[
-    aws.ec2.SecurityGroupIngressArgs(
+    ingress=[aws.ec2.SecurityGroupIngressArgs(
         protocol='tcp',
         from_port=22,
         to_port=22,
         cidr_blocks=['0.0.0.0/0']
     )],
-    egress=[
-    aws.ec2.SecurityGroupEgressArgs(
+    egress=[aws.ec2.SecurityGroupEgressArgs(
         protocol='-1',
         from_port=0,
         to_port=0,
@@ -70,14 +68,14 @@ newsg = aws.ec2.SecurityGroup("newsg", # You typed resource_name="newsg" it shou
 )
 
 # Define a new EBS volume
-my_volume = aws.ebs.Volume("my_volume", # That should be aws.ec2.Volume as you have aws.ec2 imported and you typed ebs.Volume.
+my_volume = aws.ec2.Volume("my_volume",
     availability_zone='us-east-1a',
     size=8,
     encrypted=True
 )
 
 # Create a snapshot
-my_snapshot = aws.ebs.Snapshot("my_snapshot", # There was missing comma at the end of the resource_name "my_snapshot"
+my_snapshot = aws.ec2.Snapshot("my_snapshot",
     volume_id=my_volume.id,
     description="This is a snapshot of my EBS volume")
 
@@ -88,8 +86,8 @@ instance = aws.ec2.Instance('my_instance',
     availability_zone='us-east-1a',
     vpc_security_group_ids=[newsg.id],
     subnet_id=private_subnet.id,
-    root_block_device= [aws.ec2.InstanceRootBlockDeviceArgs(
-        volume_id='my_volume.id',
+    root_block_device=[aws.ec2.InstanceRootBlockDeviceArgs(
+        volume_id=my_volume.id,
         volume_type="gp2",
         volume_size=8,
     )]
