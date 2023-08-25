e3a65121883796434732983ac194b9a629580887
Ameer ALi <114929844+Ameer-clk@users.noreply.github.com>
2023-08-25 11:54:04 +0000

    update

--- java-project/src/main/java/myproject/App.java
+++ java-project/src/main/java/myproject/App.java
@@ -1,8 +1,6 @@
 package generated_program;
 
-import com.pulumi.Context;
 import com.pulumi.Pulumi;
-import com.pulumi.core.Output;
 import com.pulumi.aws.ec2.Vpc;
 import com.pulumi.aws.ec2.VpcArgs;
 import com.pulumi.aws.ec2.Subnet;
@@ -15,7 +13,6 @@ import com.pulumi.aws.ec2.inputs.RouteTableRouteArgs;
 import com.pulumi.aws.ec2.SecurityGroup;
 import com.pulumi.aws.ec2.SecurityGroupArgs;
 import com.pulumi.aws.ec2.inputs.SecurityGroupIngressArgs;
-import com.pulumi.aws.ec2.inputs.SecurityGroupEgressArgs;
 import com.pulumi.aws.ebs.Volume;
 import com.pulumi.aws.ebs.VolumeArgs;
 import com.pulumi.aws.ebs.Snapshot;
@@ -33,13 +30,7 @@ import com.pulumi.aws.lb.TargetGroup;
 import com.pulumi.aws.lb.TargetGroupArgs;
 import com.pulumi.aws.autoscaling.Group;
 import com.pulumi.aws.autoscaling.GroupArgs;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.rmi.StubNotFoundException;
+
 
 public class App {
     public static void main(String[] args) {
@@ -47,39 +38,39 @@ public class App {
     }
 
 // Create a vpc
- Vpc my_vpc = new Vpc("my_vpc", VpcArgs.build()        
+ Vpc my_vpc = new Vpc("myVpc", VpcArgs.build()        
 .cidrBlock("10.1.0.0/16")
 .build());
 
 // Create a public subnet
-Subnet public_subnet = new Subnet("public_subnet", SubnetArgs.builder()
-.vpcId(my_vpc.id)
+Subnet public_subnet = new Subnet("publicSubnet", SubnetArgs.builder()
+.vpcId(myVpc.id)
 .cidrBlock("10.1.10.0/24")
 .availabilityZone("us-east-1a")
 .build());
 
  // Create a Private subnet
- Subnet private_subnet = new Subnet("private_subnet", SubnetArgs.builder()
-    .vpcId(my_vpc.id)
+ Subnet private_subnet = new Subnet("privateSubnet", SubnetArgs.builder()
+    .vpcId(myVpc.id)
     .cidrBlock("10.1.20.0/24")
     .availabilityZone("us-east-1b")
     .build());
 
  // Create a private subnet
-Subnet private_subnet1 = new Subnet("private_subnet1", SubnetArgs.builder()
-.vpcId(my_vpc.id)
+Subnet private_subnet1 = new Subnet("privateSubnet1", SubnetArgs.builder()
+.vpcId(myVpc.id)
 .cidrBlock("10.1.30.0/24")
 .availabilityZone("us-east-1c")
 .builder());
 
  // Create a internet gateway
-InternetGateway internet_gateway = new InternetGateway("internetgateway", InternetGatewayArgs.builder()
-.vpcId(my_vpc.id)
+InternetGateway internet_gateway = new InternetGateway("InternetGateway", InternetGatewayArgs.builder()
+.vpcId(myVpc.id)
 .build());
 
  // Create public route table and attaching public route table routes
 Routetable public_route_table = new RouteTable("publicroutetable", RouteTableArgs.builder()
-            .vpcId(my_vpc.id))
+            .vpcId(myVpc.id))
             .routes(
                 RouteTableRouteArgs.builder()
                     .cidrBlock("0.0.0.0/0")
@@ -88,15 +79,15 @@ Routetable public_route_table = new RouteTable("publicroutetable", RouteTableArg
 
  // Create Private route table with attaching pirvate route in to it
 Routetable private_route_table = new RouteTable("privateroutetable", RouteTableArgs.builder()
-.vpcId(my_vpc.id))
+.vpcId(myVpc.id))
 .route(
     RouteTableRouteArgs.builder()
     .cidrBlock("0.0.0.0/0")
-    .gatewayId(igw.id)
+    .gatewayId(InternetGateway.id)
     .build());
 
 // Create Security group
-SecurityGroup secuirty_group = new SecurityGroupRule("sg", SecurityGroupRuleArgs.builder()
+SecurityGroup secuirty_group = new SecurityGroupRule("SecurityGroup", SecurityGroupRuleArgs.builder()
             .type("ingress")
             .fromPort(22)
             .toPort(22)
@@ -119,7 +110,6 @@ SecurityGroup secuirty_group = new SecurityGroupRule("sg", SecurityGroupRuleArgs
 Volume new_volume = new Volume("newvolume", VolumeArgs.builder()
 .availabilityZone("us-east-1a")
 .size(8)
-.tags(Map.of("Name", "HelloWorld"))
 .build());
 
 // Create a EBS Snapshot
@@ -140,30 +130,35 @@ Ami example = new Ami("example", AmiArgs.builder()
     .virtualizationType("hvm")
     .build());
 
- // Create a Launch Template
-Template new_template = new LaunchTemplate("newtemplate", LaunchTemplateArgs.builder(),
-imageId("ami-08a52ddb321b32a8c")
-.instanceType("t2.micro")
-.keyName("minikube")
-.availabilityZone("us-east-1a")
-.vpcSecurityGroupIds("internetgateway.id")
-.associatePublicIpAddress(true)
-.disableApiTermination(true)
-.build());
+   // Create a Launch Template
+ LaunchTemplate new_template = new LaunchTemplate("newTemplate", LaunchTemplateArgs.builder()
+     .imageId("ami-08a52ddb321b32a8c")
+     .instanceType("t2.micro")
+     .keyName("minikube")
+     .placement(LaunchTemplatePlacementArgs.builder()
+         .availabilityZone("us-east-1a")
+         .build())
+     .networkInterfaces(LaunchTemplateNetworkInterfaceArgs.builder()
+         .securityGroups(SecurityGroup.id)
+         .associatePublicIpAddress(true)
+         .build())
+     .disableApiTermination(true)
+     .build());
 
 // Create LoadBalancer Target group
 TargetGroup test_target = new TargetGroup("testtarget", TargetGroupArgs.builder()
 .port(80)
 .protocol("HTTP")
-.vpcId(main.id())
+.vpcId(myVpc.id)
 .build());
 
  // Create an Application LoadBalancer
  LoadBalancer application_balancer = new LoadBalancer("applicationbalancer", LoadBalancerArgs.builder()
 .internal(true)
 .LoadBalancertype("application")
-.SecurityGroup(igw.id)
-.Subnet(private_subnet.id,private_subnet1.id));
+.SecurityGroup(InternetGateway.id)
+.Subnet(privateSubnet.id,privateSubnet1.id))
+.build();
 
  // Create an LoadBalancer Lister for Application LoadBalancer
 Listener Application_Listener = new Listener("applicationListener", ListenerArgs.builder()
@@ -180,10 +175,10 @@ type("forward")
 AutoScalling Autosaclling_group = new Group("autoscallinggroup", GroupArgs.builder()
 .availabilityZones("us-east-1a")
 .desiredCapacity(1)
-.maxSize(1)
+.maxSize(2)
 .minSize(1)
 .launchTemplate(GroupLaunchTemplateArgs.builder()
-    .id(newtemplate.id)
+    .id(newTemplate.id)
     .version("$Latest")
     .build())
 .build());
